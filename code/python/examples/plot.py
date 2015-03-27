fix weighting and test animations

code/python/examples/plot.py
@@ -6,6 +6,7 @@ import pylab as plt
 import os
 import genpoints
 
+# Transform a list of couples to a list
 def to_list_points (lst):
     tmp = map(lambda (x, y): [x, y], lst)
     return [item for sublist in tmp for item in sublist]
@@ -13,6 +14,8 @@ def to_list_points (lst):
 # points = np.array([0.0, 0.0, 0.5, 0.0, 0.0, 0.5, 0.2, 0.2])
 # points = np.array(to_list_points(genpoints.on_circle(10, 100)))
 points = np.array(to_list_points(list(genpoints.on_ellipse(10, 20, 100))))
+
+# Parameters
 radius = 1
 iter = 10
 timestep = 0.1
@@ -30,42 +33,52 @@ def find_min_max (points):
 xmin, xmax, ymin, ymax = find_min_max(points)
 var = 1
 
-def step_gradient_descent_perimeter (points, radius, timestep):
-    grad = unionballs.gradient_perimeter_boundary(points, radius)
+def step_gradient_descent_perimeter (points, radius,timestep, weighted = False):
+    grad = unionballs.gradient_perimeter_boundary(points, radius, weighted)
     return points - timestep * grad
 
-def step_gradient_descent_volume (points, radius, timestep):
-    grad = unionballs.gradient_volume(points, radius)
+def step_gradient_descent_volume (points, radius, timestep, weighted = False):
+    grad = unionballs.gradient_volume(points, radius, weighted)
     return points - timestep * grad
 
-def plot_points (points, filename):
+def plot_points (points, filename, iter):
     plt.figure()
     i = 0
     while i < len(points):
         plt.plot(points[i], points[i + 1], 'r+')
         i += 2
     plt.axis([xmin - var, xmax + var, ymin - var, ymax + var])
+    str = "i = " + `iter`
+    print(str)
+    plt.text((xmax - xmin)/ 2 - var, -(ymax - ymin) / 2 + var, str)
+    # plt.text(0, 0, str)
     plt.savefig(filename)
     plt.close()
 
-def gradient_descent_perimeter (points, radius, timestep):
-    os.system("rm -f perimeter*.png")
+def gradient_descent_perimeter (points, radius, timestep, fname, weighted = False):
+    print(fname)
+    os.system("rm -f " + fname + "*.png")
     ps = points
     for i in range(0, iter):
-        filename = "perimeter" + `i` + ".png"
-        plot_points(ps, filename)
-        ps = step_gradient_descent_perimeter(ps, radius, timestep)
-    os.system("convert -delay 50 -loop 0 `ls perimeter*.png | sort -k1.10n` perimeter.gif")
+        filename = fname + `i` + ".png"
+        plot_points(ps, filename, i)
+        ps = step_gradient_descent_perimeter(ps, radius, timestep, weighted)
+    l = len(fname) + 1
+    os.system("convert -delay 50 -loop 0 `ls " + fname + "*.png | sort -k1." + `l` + "n` " + fname + ".gif")
 
-def gradient_descent_volume (points, radius, timestep):
-    os.system("rm -f volume*.png")
+def gradient_descent_volume (points, radius, timestep, fname, weighted = False):
+    print(fname)
+    os.system("rm -f " + fname + "*.png")
     ps = points
     for i in range(0, iter):
-        filename = "volume" + `i` + ".png"
-        plot_points(ps, filename)
-        ps = step_gradient_descent_volume(ps, radius, timestep)
-    os.system("convert -delay 50 -loop 0 `ls volume*.png | sort -k1.7n` volume.gif")
+        filename = fname + `i` + ".png"
+        plot_points(ps, filename, i)
+        ps = step_gradient_descent_volume(ps, radius, timestep, weighted)
+    l = len(fname) + 1
+    os.system("convert -delay 50 -loop 0 `ls " + fname + "*.png | sort -k1." + `l` + "n` " + fname + ".gif")
 
-gradient_descent_perimeter(points, radius, timestep)
-gradient_descent_volume(points, radius, timestep)
+gradient_descent_perimeter(points, radius, timestep, "perimeter")
+gradient_descent_perimeter(points, radius, timestep, "perimeter-weighted", True)
+gradient_descent_volume(points, radius, timestep, "volume")
+gradient_descent_volume(points, radius, timestep, "volume-weighted", True)
 